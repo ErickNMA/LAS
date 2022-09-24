8fed2a3f5aa0913df79f109d048445f498400af5
Erick Nathan Martins Alves <ericknathancoro@hotmail.com>
2022-09-24 18:15:29 -0300

    .

--- guias123/guia_1.py
+++ guias123/guia_1.py
@@ -74,7 +74,7 @@ T0 = 27 #Condição inicial do sistema.
 #Observe que para simulação em malha aberta a função exige os seguintes parâmetros:
 #Sistema a ser simualado, vetor com o tempo de simulação, vetor sinal de controle, condição
 #inicial do sistema.
-t, y = ct.input_output_response(SYSTEM, t, u, T0) 
+t, y = ct.input_output_response(SYSTEM, t, u, T0)
 
 #Plotando o resultado da simulação-------------------------------------------------------------------
 plt.figure(1)
@@ -83,7 +83,7 @@ plt.plot(t,y,'r',label='Taq(t)')
 plt.ylabel('Temperatura (°C)')
 plt.xlim(0,tf)
 plt.legend()
-#plt.ylim(0,0.80)
+plt.ylim(20, 90)
 plt.title('Resposta temporal do sistema em malha aberta')
 plt.grid()
 plt.subplot(2,1,2)
@@ -95,6 +95,69 @@ plt.xlim(0,tf)
 plt.grid()
 plt.show()
 
+#Resposta ao degrau com 3 parâmetros:
 
+#Degrau (+5):
+#Equilíbrio:
+y0 = (Teq*np.ones(t.shape))
+#Reta tangente:
+y1 = (0.06*t + 54.83)
+A1 = ((80-54.83)/0.06)
+#Reta 0,63K:
+y2 = ((80+(0.63*5))*np.ones(t.shape))
+B1 = (((80+(0.63*5))-54.83)/0.06)
+#Reta K:
+y3 = (85*np.ones(t.shape))
+C1 = ((85-54.83)/0.06)
 
+print("\n=> Resposta ao degrau (+5°): \tA = " + str(round(A1, 4)) + "\t B = " + str(round(B1, 4)) + "\t C = " + str(round(C1, 4)))
+
+plt.figure(2)
+plt.subplot(1, 2, 1)
+plt.plot(t,y,'red',label='Taq(t)')
+plt.plot(t,y0,'black',label='Equilíbrio')
+plt.plot(t,y1,'pink',label='Tangente')
+plt.plot(t,y2,'yellow',ls='--',label='0,63k')
+plt.plot(t,y3,'blue',ls='--',label='k')
+plt.scatter(A1, 80, c='orange', linewidths=3, label='A')
+plt.scatter(B1, (80+(0.63*5)), c='purple', linewidths=3, label='B')
+plt.scatter(C1, 85, c='green', linewidths=3, label='C')
+plt.ylabel('Temperatura (°C)')
+plt.xlim(400, 530)
+plt.legend()
+plt.ylim(79, 86)
+plt.title('Resposta ao degrau com 3 parâmetros (+5°)')
+plt.grid()
+
+#Degrau (-5):
+#Reta tangente:
+y4 = (-0.08*t + 272.32)
+A2 = ((80-272.32)/-0.08)
+#Reta 0,63K:
+y5 = ((80-(0.63*5))*np.ones(t.shape))
+B2 = (((80-(0.63*5))-272.32)/-0.08)
+#Reta K:
+y6 = (75*np.ones(t.shape))
+C2 = ((75-272.32)/-0.08)
+
+print("\n=> Resposta ao degrau (-5°): \tA = " + str(round(A2, 4)) + "\t B = " + str(round(B2, 4)) + "\t C = " + str(round(C2, 4)))
+
+plt.figure(2)
+plt.subplot(1, 2, 2)
+plt.plot(t,y,'red',label='Taq(t)')
+plt.plot(t,y0,'black',label='Equilíbrio')
+plt.plot(t,y4,'pink',label='Tangente')
+plt.plot(t,y5,'yellow',ls='--',label='0,63k')
+plt.plot(t,y6,'blue',ls='--',label='k')
+plt.scatter(A2, 80, c='orange', linewidths=3, label='A')
+plt.scatter(B2, (80-(0.63*5)), c='purple', linewidths=3, label='B')
+plt.scatter(C2, 75, c='green', linewidths=3, label='C')
+plt.ylabel('Temperatura (°C)')
+plt.xlim(2400, 2520)
+plt.legend()
+plt.ylim(74, 81)
+plt.title('Resposta ao degrau com 3 parâmetros (-5°)')
+plt.grid()
+
+plt.show()
 
